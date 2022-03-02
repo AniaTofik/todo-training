8260c2a07a9f2225d98b5de2142656f1ee94a570
AniaTofik <anna.kiedel@onet.pl>
2022-03-02 19:55:17 +0100

    added nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsPageModule } from './pages/about-us.page-module';
+import { ContactPageModule } from './pages/contact.page-module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'onas',
+    loadChildren: () => AboutUsPageModule,
+  },
+  {
+    path: 'contact',
+    loadChildren: () => ContactPageModule,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
