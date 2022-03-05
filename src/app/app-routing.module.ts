458211424d0d58468ca164c80828d6bd056a9561
AniaTofik <anna.kiedel@onet.pl>
2022-03-05 10:57:39 +0100

    fix the homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
     loadChildren: () => ProjectsPageModule,
   },
   {
-    path: 'home',
+    path: '',
     loadChildren: () => HomePageModule,
   },
 ];
--- src/app/pages/home.page-module.ts
+++ src/app/pages/home.page-module.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HomePage } from './home.page';
 
-@NgModule({ imports: [CommonModule, 
-      RouterModule.forChild([
-        {
-          path: '',
-          component: HomePage,
-        }
-      ])],
-  	declarations: [HomePage],
-  	providers: [],
-  	exports: [] })
-export class HomePageModule {
-}
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage,
+      },
+    ]),
+  ],
+  declarations: [HomePage],
+  providers: [],
+  exports: [],
+})
+export class HomePageModule {}
