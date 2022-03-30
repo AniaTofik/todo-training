d91f3fdd41c23565a43303fd85e74eb1c962733d
AniaTofik <anna.kiedel@onet.pl>
2022-03-30 19:30:52 +0200

    connected route resolve in page module

--- src/app/pages/employee-details.page-module.ts
+++ src/app/pages/employee-details.page-module.ts
@@ -5,6 +5,8 @@ import { EmployeeDetailsPage } from './employee-details.page';
 import { EmployeeDetailComponentModule } from '../../../projects/team/src/lib/adapters/primary/ui/employee-detail.component-module';
 import { FirebaseEmployeesServiceModule } from '../../../projects/team/src/lib/adapters/secondary/infrastructure/firebase-employees.service-module';
 import { OurTeamComponentModule } from '../../../projects/team/src/lib/adapters/primary/ui/our-team.component-module';
+import { EmployeeIdResolverModule } from '@team';
+import { EmployeeIdResolver } from 'projects/team/src/lib/adapters/primary/ui/employee-id.resolver';
 
 @NgModule({
   imports: [
@@ -13,11 +15,15 @@ import { OurTeamComponentModule } from '../../../projects/team/src/lib/adapters/
       {
         path: ':employeeId',
         component: EmployeeDetailsPage,
+        resolve: {
+          employeeId: EmployeeIdResolver,
+        },
       },
     ]),
     EmployeeDetailComponentModule,
     FirebaseEmployeesServiceModule,
     OurTeamComponentModule,
+    EmployeeIdResolverModule,
   ],
   declarations: [EmployeeDetailsPage],
   providers: [],
