02140ae705a600179d9be832f25fc243fb1f8e55
AniaTofik <anna.kiedel@onet.pl>
2022-03-30 19:18:58 +0200

    created Route Resolver

--- projects/team/src/index.ts
+++ projects/team/src/index.ts
@@ -4,3 +4,4 @@
 export * from './lib/adapters/primary/ui/our-team.component-module';
 export * from './lib/adapters/primary/ui/create-employee.component-module';
 export * from './lib/adapters/secondary/infrastructure/in-memory-context.storage-module';
+export * from './lib/adapters/primary/ui/employee-id.resolver-module';
